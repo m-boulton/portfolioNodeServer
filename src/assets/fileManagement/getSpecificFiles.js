9147076a1d3ee4132299300077756ce2e32ce3ad
michael <boultonmichael@gmail.com>
2021-06-27 08:07:18 -0600

    testing paths

--- src/assets/fileManagement/getSpecificFiles.js
+++ src/assets/fileManagement/getSpecificFiles.js
@@ -7,30 +7,37 @@ const envSplit = DEV ? "\r" : "\n";
 
 async function getSpecificFiles(locations) {
   try {
-    const responseArray = [];
-    locations.forEach((itemPath) => {
-      const directoryContents = readdir(itemPath);
-      if (directoryContents.includes("package.json", "README.md")) {
-        const packagePath = resolve(location, "package.json");
-        const readmePath = resolve(location, "README.md");
-        return responseArray.push({ package: packagePath, readme: readmePath });
-      } else {
-        return responseArray.push(directoryContents);
-      }
-      throw `failed getting location files : ${itemPath}`;
-    });
+    const responseArray = await Promise.all(
+      locations.map((relativePath) => {
+        const packagePath = resolve(relativePath, "package.json");
+        const readmePath = resolve(relativePath, "README.md");
+        return { package: packagePath, readme: readmePath };
+      })
+    );
     return responseArray;
-    // const directoryContents = await readdir(locations);
-    // if (directoryContents.includes("package.json", "README.md")) {
-    //   const packagePath = resolve(location, "package.json");
-    //   const readmePath = resolve(location, "README.md");
-    //   const ignoredFilesArray = await readFileSync(resolvedPath, {
-    //     encoding: "utf-8",
-    //   })
-    //     .toString()
-    //     .split(envSplit);
-    //   return ignoredFilesArray;
-    // }
+
+    // const responseArray = [];
+    // locations.forEach((itemPath) => {
+    //   const directoryContents = await readdir(itemPath);
+    //   if (directoryContents.includes("package.json", "README.md")) {
+    //     const packagePath = resolve(location, "package.json");
+    //     const readmePath = resolve(location, "README.md");
+    //     return responseArray.push({ package: packagePath, readme: readmePath });
+    //   }
+    //   throw `failed getting location files : ${itemPath}`;
+    // });
+    // return responseArray;
+    // // const directoryContents = await readdir(locations);
+    // // if (directoryContents.includes("package.json", "README.md")) {
+    // //   const packagePath = resolve(location, "package.json");
+    // //   const readmePath = resolve(location, "README.md");
+    // //   const ignoredFilesArray = await readFileSync(resolvedPath, {
+    // //     encoding: "utf-8",
+    // //   })
+    // //     .toString()
+    // //     .split(envSplit);
+    // //   return ignoredFilesArray;
+    // // }
   } catch (error) {
     return error;
   }
