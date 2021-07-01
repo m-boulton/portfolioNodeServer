ac011cf68cf1a96a54c239155613cef175b14e3b
michael <boultonmichael@gmail.com>
2021-07-01 12:23:54 -0600

    updating project query names to match package.json name value

--- src/assets/fileManagement/projectLocation.js
+++ src/assets/fileManagement/projectLocation.js
@@ -10,10 +10,10 @@ const {
 } = process.env;
 
 function projectLocation(location) {
-  if (location === "amdFrontend") return amdFrontend;
-  if (location === "amdBackend") return amdBackend;
-  if (location === "portfolioFrontend") return portfolioFrontend;
-  if (location === "portfolioBackend") return portfolioBackend;
+  if (location === "amd_product_display_website") return amdFrontend;
+  if (location === "amd_product_api_node_server") return amdBackend;
+  if (location === "portfolio_display_website") return portfolioFrontend;
+  if (location === "portfolio_api_node_server") return portfolioBackend;
   if (location === "parent") return parent;
   if (location === "test") return test;
   throw `project location doesnt exist : ${location}`;
